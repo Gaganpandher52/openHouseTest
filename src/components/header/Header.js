64d492d35b0f2204e2672828bff7ed9d8d433ae8
gaganpandher52 <gaganpandher52@gmail.com>
2019-11-22 21:47:40 -0700

    added header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='nav-bar-header'>
+        <h4>OpenHouse.Ai</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
