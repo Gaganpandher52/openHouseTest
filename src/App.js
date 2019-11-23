392ac545d4e218bf4eff05d32a42089ce7a17f39
gaganpandher52 <gaganpandher52@gmail.com>
2019-11-22 22:52:09 -0700

    added the logic for names

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header'
+import CommunityInfo from './components/communityName/CommunityInfo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CommunityInfo/>
     
     </div>
   );
--- src/components/communityName/CommunityInfo.js
+++ src/components/communityName/CommunityInfo.js
@@ -4,19 +4,58 @@ class CommunityInfo extends Component {
   constructor(){
     super()
     this.state = {
-        info = []
+      error:null,
+      isLoaded:false,
+      info : []
 
     }
+  }
+  getName = () => {
+    fetch('/https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/communities')
+    .then(res => res.json())
+    .then(info => this.setState({ info }));
+
+  }
+
+  componentDidMount(){
+    fetch('https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/communities')
+    .then(res => res.json())
+    .then((result) => {
+      this.setState({
+        isLoaded:true,
+        info: result
+      });
+    },
+    //handle error here
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    });
   }
 
 
 
   render() {
-    return (
-      <div>
-        
-      </div>
-    );
+    const {error, isLoaded, info} = this.state;
+    if(error){
+      return <div>Error: {error.message}</div>
+    } else if(!isLoaded){
+      return <div>Loading...</div>
+    } else{
+      return (
+        <ul>
+          {info.map(name => (
+            <li key={info.name}>
+              {name.name} 
+            </li>
+          ))}
+        </ul>
+      );
+      
+    }
+    
   }
 }
 
