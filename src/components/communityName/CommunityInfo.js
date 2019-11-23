31a212ab9bc3ea3898f254467bcdcdca9514b650
gaganpandher52 <gaganpandher52@gmail.com>
2019-11-22 21:52:02 -0700

    created new community comp

---
+++ src/components/communityName/CommunityInfo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class CommunityInfo extends Component {
+  constructor(){
+    super()
+    this.state = {
+        info = []
+
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default CommunityInfo;
